d347ce596e9ef60f65a84f552f7fc63ba49d23d4
majuliah <majulialcarvalho@gmail.com>
2026-08-28 18:10:59 -0300

    Adicionando entidade Curso e enum TipoCurso

---
+++ sistema-de-gestao-de-faculdade/TipoCurso.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sistema_de_gestao_de_faculdade
+{
+    public enum TipoCurso
+    {
+        GRADUACAO,
+        POS_GRADUACAO
+    }
+}
