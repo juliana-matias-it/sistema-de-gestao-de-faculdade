62bb9da23ba37821de1b40e420094675975f4158
majuliah <majulialcarvalho@gmail.com>
2026-08-28 18:28:23 -0300

    Inserindo a consulta de cursos

--- sistema-de-gestao-de-faculdade/Services/FaculdadeService.cs
+++ sistema-de-gestao-de-faculdade/Services/FaculdadeService.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using sistema_de_gestao_de_faculdade;
 using System.Collections.Generic;
-using System.Text;
+using static System.Console;
+using System.Linq;
 
-namespace sistema_de_gestao_de_faculdade.Services
+
+public class FaculdadeService
 {
-    internal class FaculdadeService
+    private List<Curso> _cursos;
+    public FaculdadeService()
     {
+        _cursos = new List<Curso>();
+    }
+    public void CadastrarCurso(string codigo, string nome, TipoCurso tipoCurso)
+    {
+        if (_cursos.Any(curso => curso.Codigo == codigo))
+        {
+            Console.WriteLine("Já existe um curso com esse código.");
+            return;
+        }
+
+        Curso curso = new Curso(codigo, nome, tipoCurso);
+        _cursos.Add(curso);
+    }
+    public void ConsultarCursos()
+    {
+        if (_cursos.Count == 0)
+        {
+            Console.WriteLine("Nenhum curso cadastrado.");
+            return;
+        }
+
+        foreach (Curso curso in _cursos)
+        {
+            WriteLine("==============================");
+            WriteLine($"Código: {curso.Codigo}");
+            WriteLine($"Nome: {curso.Nome}");
+            WriteLine($"Tipo: {curso.TipoCurso}");
+            WriteLine("==============================");
+        }
     }
 }
