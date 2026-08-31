b86dc1e855a67ed5459e69ddba5f266392e8b6ae
Erika Lima <eralmlima@outlook.com>
2026-08-31 14:15:20 -0300

    refactor: remove imports desnecessrio na entidade Professor

--- sistema-de-gestao-de-faculdade/Entity/Professor.cs
+++ sistema-de-gestao-de-faculdade/Entity/Professor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace sistema_de_gestao_de_faculdade.Entity
+﻿namespace sistema_de_gestao_de_faculdade.Entity
 {
     public class Professor : Pessoa
     {
