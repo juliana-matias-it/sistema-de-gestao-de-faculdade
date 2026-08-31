c81e8f8d2eb61b41379f3289ac2043461db54d64
juliana-matias-it <matiasjulianait@gmail.com>
2026-08-31 15:19:33 -0300

    Altera set de Matricula para private set

--- sistema-de-gestao-de-faculdade/Entity/Matricula.cs
+++ sistema-de-gestao-de-faculdade/Entity/Matricula.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace sistema_de_gestao_de_faculdade.Entity
+﻿namespace sistema_de_gestao_de_faculdade.Entity
 {
     public class Matricula
     {
-        public Aluno Aluno { get; set; }
-        public Curso Curso { get; set; }
-        public Boletim Boletim { get; set; }
+        public Aluno Aluno { get; private set; }
+        public Curso Curso { get; private set; }
+        public Boletim Boletim { get; private set; }
 
         public Matricula(Aluno aluno, Curso curso)
         {
